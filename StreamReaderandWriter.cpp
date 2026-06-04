6e204d687096b18177d4afafa19577559cb46bfa
Cruelasss <irula5198@gmail.com>
2026-06-04 16:03:20 +0700

    menambahkan fungsi main dan logika penulisan ke file.

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 using namespace std;
 
-int main() 
+int main()
 {
-    
-};
+
+    //deklarasi variabel baris untuk menyimpan data string
+    string baris;
+
+    //membuka file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+  
+   
+
+}
