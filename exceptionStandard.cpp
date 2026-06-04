fad352c8b2bfbf11213c1d970f3e9efd549a5693
Cruelasss <irula5198@gmail.com>
2026-06-04 16:26:54 +0700

    menambahkan penanganan exception dan pesan kesalahan untuk meningkatkan stabilitas program

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -4,4 +4,21 @@
 using namespace std;
 
 int main(){
-  
+    //menampilkan pesan awal program
+  cout << "Awal program" << endl; 
+    try
+    {
+        //membuat array dengan 3 elemen
+        array <int, 3> data = { 1, 2, 3 };
+        //mengakses elemen ke-5 dari array, yang berada di luar batas
+        cout << data.at(5) << endl;
+    }
+    catch (exception& e)
+    {
+        //menangkap dan menampilkan pesan kesalahan yang terjadi
+        cout << e.what() << endl;
+    }
+    // program tetap berjalan meskipun terjadi exception
+    cout << "Baris program terakhir" << endl; //penanda 2
+    return 0;
+}
