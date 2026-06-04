dfdcfea61600a6a19c0b369e4b87bd8b12d4b5cd
Cruelasss <irula5198@gmail.com>
2026-06-04 16:18:59 +0700

    menambahkan pengecekan untuk memastikan file dapat dibuka sebelum membaca isinya

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -24,5 +24,14 @@ int main(){
   ifstream infile;
   infile.open(NamaFile + ".txt", ios::in);
   cout << endl << ">= Membuka dan membaca file" << endl;
-    
+    if (infile.is_open())
+    {
+      while (getline(infile, baris))
+        {
+            cout << baris << endl;
+        }
+      infile.close();
+    }
+    else cout << "Unable to open file";
+    return 0;
 }
