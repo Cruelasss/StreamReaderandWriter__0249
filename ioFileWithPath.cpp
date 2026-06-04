71937c82ffea09d185f0c21c7cb41c6098618bef
Cruelasss <irula5198@gmail.com>
2026-06-04 16:20:27 +0700

    menambahkan komentar untuk meningkatkan pemahaman kode dan struktur

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -4,32 +4,44 @@
 using namespace std;
 
 int main(){
+    //deklarasi variabel baris untuk menyimpan data string dan NamaFile untuk menyimpan nama file yang akan dibuat
   string baris, NamaFile;
 
   cout << "Masukkan nama file : ";
   cin >> NamaFile;
-  
+  //membuka file dalam mode menulis
   ofstream outfile;
+//menunjuk ke sebuah nama file dengan ekstensi .txt dan mode output
   outfile.open(NamaFile + ".txt", ios::out);
 
   cout << ">= Menulis file, \'q\' untuk keluar" << endl;
-
+//unlimited loop untuk menulis file
   while (true){
     cout << "- ";
+    //mendapat setiap karakter dalam satu baris
     getline(cin, baris);
+    //kondisi false jika baris hanya berisi  karakter q
     if (baris == "q") break;
+    //menulis dan memasukkan nilai dari variabel baris ke dalam file
     outfile << baris << endl;
   }
+  //menutup file setelah penulisan selesai
   outfile.close();
+  //membuka file dalam mode membaca
   ifstream infile;
+  //menunjuk ke sebuah file
   infile.open(NamaFile + ".txt", ios::in);
+
   cout << endl << ">= Membuka dan membaca file" << endl;
-    if (infile.is_open())
+//kondisi jika file ada  
+  if (infile.is_open())
     {
+    //perulangan untuk membaca setiap baris
       while (getline(infile, baris))
         {
             cout << baris << endl;
         }
+        //menutup file setelah pembacaan selesai
       infile.close();
     }
     else cout << "Unable to open file";
